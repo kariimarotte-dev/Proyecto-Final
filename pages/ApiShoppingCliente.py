646dc5e73eef35d6606393529c2785a2e595bc36
Kari Marotte <kariimarotte@gmail.com>
2026-06-20 16:00:16 -0300

    hice un cliente de API que me permite hacer consultas GET para obtener un post o todos los posts de una tienda mediante sus endpoinsts

---
+++ pages/ApiShoppingCliente.py
@@ -0,0 +1,29 @@
+import requests 
+
+class ApiShoppingClient:
+    
+    def __init__(self):
+    
+        self.URL_BASE = "https://shopping-kohl-tau.vercel.app"
+
+    def get_posts(self, post_id):
+        """
+        Este método hace una petición GET para traer un post específico usando su ID.
+        Ejemplo: Si pasás post_id = 1, buscará en /posts/1
+        """
+        # ruta exacta combinando la base con el endpoint /posts/ y el ID
+        url_completa = f"{self.URL_BASE}/posts/{post_id}"
+        
+        # Hacemos el llamado GET y guardamos lo que nos responde el servidor
+        respuesta = requests.get(url_completa)
+        
+        
+        return respuesta
+
+    def get_todos_los_posts(self):
+        """
+        Este método trae la lista completa de publicaciones de la tienda.
+        Mapea directamente al endpoint GET /posts
+        """
+        url_completa = f"{self.URL_BASE}/posts"
+        return requests.get(url_completa)
